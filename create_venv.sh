9ed746cd6780a772fa6ecc2c9c4af3f5bf5f95a3
salvatorelionetti <salvatorelionetti@yahoo.it>
2025-12-31 17:52:10 +0100

    Create create_venv.sh
    Using jetpack 6.0

---
+++ create_venv.sh
@@ -0,0 +1,10 @@
+set -eux
+pip install ../pytorch/JetPack6.0_L4T\ R36.2_R36.3_CUDA_12.2/torch-2.3.0-cp310-cp310-linux_aarch64.whl
+pip install ../pytorch/JetPack6.0_L4T\ R36.2_R36.3_CUDA_12.2/torchaudio-2.3.0+952ea74-cp310-cp310-linux_aarch64.whl
+pip install numpy==1.22
+pip install openai-whisper
+pip install pyaudio
+#pip install faster-whiper
+#pip install faster-whiper-cli
+pip install ollama
+pip install TTS
